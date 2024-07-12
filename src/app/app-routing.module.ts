df8aa7ca8fb8fe3ea7007b10752f0523851547e3
Ilyasse <developer.elias.essadak@gmail.com>
2024-07-12 17:02:25 +0200

    Creando nav sencillo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RutinasComponent } from './menu/rutinas/rutinas.component';
+import { CalendarioComponent } from './menu/calendario/calendario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rutinas', component: RutinasComponent },
+  { path: 'calendario', component: CalendarioComponent },
+  { path: '', redirectTo: '/rutinas', pathMatch: 'full' } // Ruta por defecto
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { RutinasComponent } from './menu/rutinas/rutinas.component';
+import { CalendarioComponent } from './menu/calendario/calendario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    RutinasComponent,
+    CalendarioComponent
   ],
   imports: [
     BrowserModule,
